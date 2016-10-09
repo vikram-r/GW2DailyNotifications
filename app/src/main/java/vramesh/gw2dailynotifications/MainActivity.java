603c777e3820e35ecd29c1e6ce74bd0189e3a4a1
Vikram Ramesh <vikram132@gmail.com>
2016-10-08 22:42:23 -0700

    Added simple action bar

--- app/src/main/java/vramesh/gw2dailynotifications/MainActivity.java
+++ app/src/main/java/vramesh/gw2dailynotifications/MainActivity.java
@@ -2,12 +2,40 @@ package vramesh.gw2dailynotifications;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "GW2DailyNotifications";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.action_bar_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Log.i(TAG, "Settings Clicked!");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
